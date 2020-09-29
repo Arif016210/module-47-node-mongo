951796cfccc2ed5b0485e5c1784e68fefcaa79db
Arif016210 <arifulislamwasim2017@gmail.com>
2020-09-29 12:38:42 +0600

    1-5 part added

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send("Hello, world!");
-})
 
+const users = ["Ariful", "Jasim", "Jasmin", "Salim", "Sufia", "Karim"];
+
+app.get('/', (req, res) => {
+    const products = {
+        name: "laptop",
+        price: "41000",
+    }
+    res.send(products);
+});
+
+app.get('/product/laptop', (req, res) => {
+    res.send({ name: 'laptop', price: '41000', quantity: 5 });
+});
+
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({ id, name });
+
+});
 
 app.listen(8080, () => {
     console.log("Server Port 8080");
-})
+});
